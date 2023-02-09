5316d4092b0ed73ef28f52910b775baf6f49dcff
jech13 <103664029+jech13@users.noreply.github.com>
2023-02-09 17:29:41 +0800

    "asdasd"

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -188,7 +188,7 @@
             <div class="p-4 pt-3 pb-1">
                 <div class="row bg-white rounded p-2 align-items-center">
                     <h4 class="col fs-5 m-0 fw-bolder">Fixed Assets Information Management</h4>
-                    <div class="col d-flex gap-3">
+                    <div class="col d-flex gap-3 justify-content-between ">
 
                         <livewire:search />
                         {{-- <form class="d-flex">
@@ -212,7 +212,7 @@
                             type="button">
                             <span class="material-icons my-primary">notifications</span>
                         </button> --}}
-                        <div class="d-flex gap-0 align-items-center profile ">
+                        <div class="d-flex gap-0 align-items-center profile  ">
                             <div class="row">
                                 <h1 class="mb-0 fw-semibold">{{ Auth::user()->getName() }}</h1>
                                 <small class="text-muted">{{ Auth::user()->getPosition()->position }}</small>
